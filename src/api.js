aa26f514e5e419121baa1b29436f0fd49edae6a8
SAK74 <sergijks@gmail.com>
2022-03-27 19:18:17 +0200

    update api_request

--- src/api.js
+++ src/api.js
@@ -1,32 +1,42 @@
 import axios from "axios";
 
-function get(config) {
+const defaultConfig = {
+  headers: {
+    "Content-Type": "application/json"
+  }
+};
+
+function request(config) {
+  config.data = config.data ? JSON.stringify(config.data) : null;
   return axios(config)
-    .then((resp) => resp.data)
-    .catch((err) => {
-      console.log(err);
-      throw Error(err.message);
-    });
-}
-function patch(data, config) {
-  // const headers = new Headers(config.headers);
-  // config.headers = headers.append("Content-Type", "application/json");
-  config.headers = { ...config.headers, "Content-Type": "application/json" };
-  // console.log("config: ", { ...config });
-  return axios({
-    // ...defaultConfig,
-    ...config,
-    method: "PATCH",
-    data: JSON.stringify(data)
-  })
     .then((resp) => resp.data)
     .catch((err) => {
       if (err.response) {
         console.log("err.response: ", err.response);
-        throw Error(err.response.error.message);
+        throw Error(err.response.data.error.message);
       }
       console.log(err);
       throw Error(err.message);
     });
 }
-export default { get, patch };
+function get(config) {
+  return request(config);
+}
+function patch(data, config) {
+  config.headers = { ...config.headers, ...defaultConfig.headers };
+  return request({
+    ...config,
+    method: "PATCH",
+    data
+  });
+}
+function post(data, config) {
+  config.headers = { ...config.headers, ...defaultConfig.headers };
+  return request({
+    ...config,
+    method: "POST",
+    data
+  });
+}
+
+export default { get, patch, post };
