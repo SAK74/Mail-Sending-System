7d119961ec902a0dd37894d2c56d03b94dfd776e
SAK74 <sergijks@gmail.com>
2022-04-12 23:11:08 +0200

    visualised adding subscr.

--- src/components/subscribers/addSubscriber/index.js
+++ src/components/subscribers/addSubscriber/index.js
@@ -1,54 +1,88 @@
-// import "./addSubscriber.css";
-import { useForm } from "react-hook-form";
+import { Button, CircularProgress, Paper, Snackbar, styled, TextField as MuiTextField } from "@mui/material";
+import { useController, useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import validator from "validator";
 import { addItem } from "../../../features/makeAirtableRequest";
 import { _addSubscriber } from "../../../pages/subscribers/subscribersSlice";
-import { Add } from "./styled";
+import AddIcon from "@mui/icons-material/Send";
+import { memo, useState } from 'react';
+
+const TextField = styled(({ name, control, rules, ...rest }) => {
+  // console.log("props: ", props);
+  const { field:
+    { ref, onChange, onBlur, value },
+    fieldState:
+    { invalid, error }
+  } = useController({ name, control, rules, defaultValue: "" });
+  return <MuiTextField {...rest}
+    name={name}
+    value={value}
+    required
+    size="small"
+    label={name}
+    error={invalid}
+    helperText={invalid && error.message}
+    inputRef={ref}
+    inputProps={{ ...onBlur, onChange }}
+  />
+})(({ theme }) => {
+  console.log("theme: ", theme);
+  return {
+    margin: theme.spacing(1)
+  }
+});
 
 function AddSubscriber() {
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting, isSubmitSuccessful },
-    reset
+    reset,
+    control
   } = useForm({
-    mode: "onChange"
+    mode: "all"
   });
   const onSubmit = async (data) => {
     await addItem("subscribers")(data)
       .then((data) => {
         reset();
+        setOpen(true);
         dispatch(_addSubscriber(data));
       });
   };
   return (
-    <Add>
-      <h3>Add subscriber</h3>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label>
-          Name:
-          <input {...register("name", { required: "Name is required!" })} />
-        </label>
-        {errors?.name && <div className="error">{errors.name.message}</div>}
-        <label>
-          E-mail:
-          <input
-            {...register("email", {
-              validate: (val) => validator.isEmail(val) || "Wrong E-mail format"
-            })}
-          />
-        </label>
-        {errors?.email && <div className="error">{errors.email.message}</div>}
-        <button type="submit">Dodaj</button>
-        {isSubmitting && <div>Pending</div>}
-        {isSubmitSuccessful && !isSubmitting && (
-          <div>Subscriber added successfully!</div>
-        )}
+    <Paper sx={{ p: 2 }}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <TextField
+          name="name"
+          control={control}
+          rules={{ required: "Name is required!" }} />
+        <TextField
+          name="email"
+          control={control}
+          rules={{ required: "E-mail is required!", validate: (val) => validator.isEmail(val) || "Wrong E-mail format" }}
+        />
+        <Button
+          variant="outlined"
+          type="submit"
+          children="Add"
+          endIcon={isSubmitting ? <CircularProgress size={15} /> : <AddIcon />}
+          size="small"
+          sx={{ ml: 8 }}
+        />
+        <Snackbar
+          open={open}
+          message="Subscriber has been added successfully!"
+          autoHideDuration={5000}
+          onClose={() => setOpen(false)}
+          anchorOrigin={{ horizontal: "center", vertical: "top" }}
+          transitionDuration={1000}
+        />
       </form>
-    </Add>
+    </Paper>
   );
 }
 
-export default AddSubscriber;
+export default memo(AddSubscriber);
