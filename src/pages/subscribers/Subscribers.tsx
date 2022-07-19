34842da696f0f109f02d631171d76549796e7ffb
SAK74 <sergijks@gmail.com>
2022-07-20 01:21:53 +0200

    Update Subscribers.tsx

--- src/pages/subscribers/Subscribers.tsx
+++ src/pages/subscribers/Subscribers.tsx
@@ -3,7 +3,7 @@ import { AddSubscriber, SubscribersList } from "../../components/subscribers";
 import { selectAllMails } from "../mails/mailsSlice";
 import { Box, IconButton, Collapse } from "@mui/material";
 import { ExpandMore, ExpandLess } from '@mui/icons-material';
-import MailToSend from '../../components/mails/MailTosend';
+import MailToSend from '../../components/mails/mailTosend';
 import { useReduxSelector } from '../../store';
 
 function Subscribers() {
