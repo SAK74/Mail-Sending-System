f56aef380e08d826764b465abd258a59914eaace
SAK74 <sergijks@gmail.com>
2022-09-09 19:21:20 +0200

    fix(server): env.PORT

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const PORT = "4000";
+const PORT = process.env.PORT || "4000";
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../client/build")));
 }
